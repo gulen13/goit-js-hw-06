1504533f318bf1af0ada66ab5112a88cf368b3dd
gulen13 <vitaliy.gulen@gmail.com>
2023-02-25 22:19:18 +0200

    10 task done

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputNum = document.querySelector('#controls>input')
+const btnCreate = document.querySelector('button[data-create]')
+const btnDestroy = document.querySelector('button[data-destroy]')
+const boxes = document.querySelector('#boxes')
+
+const createBoxes = amount => {
+  const elementsToAdd = []
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div')
+    div.style.width = `${30 + 10 * i}px`
+    div.style.height = `${30 + 10 * i}px`
+    div.style.background = getRandomHexColor()
+    elementsToAdd.push(div)
+  }
+  return elementsToAdd
+}
+
+const destroyBoxes = () => {
+  boxes.innerHTML = ''
+}
+
+btnCreate.addEventListener('click', () => {
+  let boxesToAdd = createBoxes(inputNum.value)
+  boxes.append(...boxesToAdd)
+})
+
+btnDestroy.addEventListener('click', () => {
+  destroyBoxes()
+  inputNum.value = ''
+})
